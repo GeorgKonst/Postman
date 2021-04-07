5fe842e4319c852a9514c87980c50f03860a4093
Georgy Levochkin <aspect71rus@gmail.com>
2021-04-07 16:28:01 +0300

    Fail

--- src/test/java/ru/netology/rest/TestApi.java
+++ src/test/java/ru/netology/rest/TestApi.java
@@ -19,7 +19,7 @@ public class TestApi {
                 .post("/post")
                 // Проверки
                 .then()
-                .statusCode(200);
-                //.body("data", equalTo("Postman"));
+                .statusCode(200)
+                .body("data", equalTo("Fail"));
     }
 }
