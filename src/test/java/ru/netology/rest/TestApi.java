4976662dc661770035ef14810d56e729a94fcec3
Georgy Levochkin <aspect71rus@gmail.com>
2021-04-07 16:25:19 +0300

    v1

---
+++ src/test/java/ru/netology/rest/TestApi.java
@@ -0,0 +1,25 @@
+package ru.netology.rest;
+
+import org.junit.jupiter.api.Test;
+
+import static io.restassured.RestAssured.given;
+import static org.hamcrest.Matchers.equalTo;
+
+public class TestApi {
+    @Test
+    void shouldPut() {
+
+        // Given - When - Then
+        // Предусловия
+        given()
+                .baseUri("https://postman-echo.com")
+                .body("Postman")
+                // Выполняемые действия
+                .when()
+                .post("/post")
+                // Проверки
+                .then()
+                .statusCode(200);
+                //.body("data", equalTo("Postman"));
+    }
+}
